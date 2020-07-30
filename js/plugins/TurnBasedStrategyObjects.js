c2be13326c03b6800956b20b1cb44b2a8e891968
Darlos9D <Darlos9D@gmail.com>
2020-07-30 00:30:31 -0400

    more inventory functions
    Added getStash*Count() functions so variables can be passed in as item ids to find items in the stash.

--- js/plugins/TurnBasedStrategyObjects.js
+++ js/plugins/TurnBasedStrategyObjects.js
@@ -543,6 +543,18 @@
 		$gameMap.setShouldCloseGoldWindow();
 	};
 	
+	Game_System.prototype.getStashItemCount = function(itemId) {
+		return $gameParty.numItems($dataItems(itemId));
+	};
+	
+	Game_System.prototype.getStashWeaponCount = function(itemId) {
+		return $gameParty.numItems($dataWeapons(itemId));
+	};
+	
+	Game_System.prototype.getStashArmorCount = function(itemId) {
+		return $gameParty.numItems($dataArmors(itemId));
+	};
+	
 	Game_System.prototype.getPartyItemCount = function(itemId) {
 		var count = 0;
 		for(let i = 0; i < $gameParty.size(); i++) {
